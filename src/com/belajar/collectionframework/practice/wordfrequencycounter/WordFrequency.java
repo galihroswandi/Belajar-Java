584f034f7294b18371a282a789cf4ebf79859ca5
galihroswandi <galihroswandi25@gmail.com>
2026-01-20 07:36:04 +0700

    feat: exercise 2, Word Frequency Count

---
+++ src/com/belajar/collectionframework/practice/wordfrequencycounter/WordFrequency.java
@@ -0,0 +1,39 @@
+package com.belajar.collectionframework.practice.wordfrequencycounter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class WordFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==== WORD FREQUENCY COUNTER ====\n");
+        System.out.print("Enter a text or paragraph: ");
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
+
+        HashMap<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+
+        System.out.println("\n╔════════════════════════╗");
+        System.out.println("║ WORD FREQUENCY COUNTER ║");
+        System.out.println("╠════════════════════════╣");
+        System.out.printf("║ %6s  ║  %5s \n", "STRING", "COUNT");
+        System.out.println("╠════════════════════════╣");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.printf("║ %s     %d \n", entry.getKey(), entry.getValue());
+        }
+        System.out.println("╚════════════════════════╝");
+
+        scanner.close();
+    }
+}
